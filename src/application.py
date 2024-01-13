aba9a20452efe1dcced296fb55e751e7e15f9c0e
Pedro J Colon <pcolon901@gmail.com>
2024-01-12 23:00:25 -0500

    Added application.py and downloader.py. Basic GUI front-end is working so far. Will work on back-end with getting download functionality going.

--- src/application.py
+++ src/application.py
@@ -1,16 +1,52 @@
 import customtkinter
 
 
+# TODO: Allow for right click event to create up pop window
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("1280x720")
+        # System Settings
 
-        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
-        self.button.pack(padx=20, pady=20)
+        # TODO: Allow window to be resizable AND have all items in the app change size depending on how much was changed
+        self.geometry("1280x720")
+        customtkinter.set_appearance_mode("dark")
+        self.title("Simple Youtube Downloader")
+        self.resizable(True, True)
+
+
+        self.insert_text = customtkinter.CTkLabel(self, text="Insert Youtube Link Down Below")
+        self.insert_text.pack(padx=10, pady=10)
+
+        url_text = customtkinter.StringVar()
+        self.link = customtkinter.CTkEntry(self, width=350, height=40, textvariable=url_text)
+        self.link.pack(padx=10, pady=10)
+
+        # Dropdown menu for downloading Audio or Video
+        self.options = customtkinter.StringVar(value="Download Video")
+        self.option_menu = customtkinter.CTkOptionMenu(self, values=["Download Video",
+                                                                     "Download Audio"],
+                                                       command=self.download_options,
+                                                       variable=self.options)
+
+        self.option_menu.pack(padx=15, pady=15)
+
+        # self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
+        # self.button.pack(padx=20, pady=20)
+
+        self.progress_bar = customtkinter.CTkProgressBar(self)
+        self.progress_bar.set(0)
+        self.progress_bar.pack(fill="x", padx=50, pady=0)
+
+        # Display video thumbnail underneath progress bar
+        #TODO: Show Thumbnail of youtube video
+        #thumbnail = customtkinter.CTkImage()
+        #image_label = customtkinter.CTkLabel(self, image=thumbnail, text="")
+
+        self.protocol("WM_DELETE_WINDOW")
+
+    def download_options(self, options):
+        pass
 
     def button_callback(self):
         print("button clicked")
-
-
-
