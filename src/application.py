f465bd52bd1048a282f22af4fbc5286ca4f1c795
Pedro J Colon <pcolon901@gmail.com>
2024-01-13 23:16:13 -0500

    Able to download single videos, audio and playlists. Exception handling given to ensure that program can work without issue. Removed progress bar due to it not updating as it should. Shows thumbnail of video downloaded

--- src/application.py
+++ src/application.py
@@ -1,25 +1,33 @@
 import customtkinter
 from src.downloader import Downloader
+from src.downloader import Status
+from PIL import Image
+import urllib.request
+from io import BytesIO
 
 # TODO: Allow for right click event to create up pop window
+
+WIDTH: int = 1280
+HEIGHT: int = 720
 
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         # System Settings
-
-        # TODO: Allow window to be resizable AND have all items in the app change size depending on how much was changed
-        self.geometry("1280x720")
+        self.geometry(str(WIDTH) + "x" + str(HEIGHT))
         customtkinter.set_appearance_mode("dark")
         self.title("Simple Youtube Downloader")
         self.resizable(True, True)
+        self.error = "No Youtube Video Downloaded Yet..."
 
-        self.option_values = ["Download Video", "Download Audio(mp3)", "Download Audio(wav)"]
+        self.option_values = ["Download Video", "Download Audio (mp3)", "Download Audio (wav)",
+                              "Download Playlist (Video)"]
 
         self.insert_text = customtkinter.CTkLabel(self, text="Insert Youtube Link Down Below")
         self.insert_text.pack(padx=10, pady=10)
 
+        # Gets url text from user, This will be used in order to get the youtube videos specified
         url_text = customtkinter.StringVar()
         self.link = customtkinter.CTkEntry(self, width=350, height=40, textvariable=url_text)
         self.link.pack(padx=10, pady=10)
@@ -28,31 +36,58 @@ class App(customtkinter.CTk):
         self.options = customtkinter.StringVar(value="Download Video")
         self.option_menu = customtkinter.CTkOptionMenu(self, values=[self.option_values[0],
                                                                      self.option_values[1],
-                                                                     self.option_values[2]],
+                                                                     self.option_values[2],
+                                                                     self.option_values[3]],
                                                        command=self.download_options,
                                                        variable=self.options)
 
         self.option_menu.pack(padx=15, pady=15)
 
-        # self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
-        # self.button.pack(padx=20, pady=20)
+        # This was made in order to tell the user if the video is Downloaded, No Url was provided, or unavailable
+        self.error_text = customtkinter.CTkLabel(self, text=self.error)
+        self.error_text.pack(padx=10, pady=5)
 
-        self.progress_bar = customtkinter.CTkProgressBar(self)
-        self.progress_bar.set(0)
-        self.progress_bar.pack(fill="x", padx=50, pady=0)
-
-        # Display video thumbnail underneath progress bar
-        #TODO: Show Thumbnail of youtube video
-        #thumbnail = customtkinter.CTkImage()
-        #image_label = customtkinter.CTkLabel(self, image=thumbnail, text="")
+        # Display video thumbnail underneath error_text.
+        # Because we have no set image, we leave this blank and only update it once the video is downloaded
+        self.image_label = customtkinter.CTkLabel(self, text="")
+        self.image_label.pack(padx=10, pady=10)
 
         self.protocol("WM_DELETE_WINDOW")
 
     def download_options(self, options):
         download = Downloader()
+        download_status = Status.NONE
         if options == self.option_values[0]:
-            download.download_video(self.link.get())
+            download_status = download.download_video(self.link.get())
         elif options == self.option_values[1]:
-            download.download_audio(self.link.get(), ".mp3")
+            download_status = download.download_audio(self.link.get(), ".mp3")
         elif options == self.option_values[2]:
-            download.download_audio(self.link.get(), ".wav")
+            download_status = download.download_audio(self.link.get(), ".wav")
+        elif options == self.option_values[3]:
+            download_status = download.download_playlist(self.link.get())
+
+        self.update_text(download_status)
+        self.show_thumbnail(download.video.thumbnail_url)
+
+    def update_text(self, download_status):
+        match download_status:
+            case Status.SUCCESS:
+                self.error = "Download Success!"
+                self.error_text.configure(text=self.error, text_color="LightGreen")
+            case Status.UNAVAILABLE:
+                self.error = "Download Failed..."
+                self.error_text.configure(text=self.error, text_color="LightRed")
+            case Status.NO_URL:
+                self.error = "No Url Provided..."
+                self.error_text.configure(text=self.error, text_color="LightYellow")
+
+    def show_thumbnail(self, url):
+        get_url = urllib.request.urlopen(url)
+        url_data = get_url.read()
+        get_url.close()  # Once we are done reading, close the url request
+        # In case of different width and height values, this will automatically downsize the image to fit as thumbnail
+        thumbnail_size_width: int = int(WIDTH / 2)
+        thumbnail_size_height: int = int(HEIGHT / 2)
+        img = customtkinter.CTkImage(dark_image=Image.open(BytesIO(url_data)),
+                                     size=(thumbnail_size_width, thumbnail_size_height))
+        self.image_label.configure(image=img)
--- src/downloader.py
+++ src/downloader.py
@@ -1,28 +1,65 @@
 import os
+from enum import Enum
 from pytube import YouTube
+from pytube import Playlist
+from pytube.exceptions import VideoUnavailable
+
+
+# Created so user can be updated on the status of the video, whether it succeeded or is unavailable
+class Status(Enum):
+    SUCCESS = 0
+    UNAVAILABLE = 1
+    NO_URL = 2
+    NONE = 3
 
 
 class Downloader():
     def __init__(self):
         super().__init__()
         self.video = None
+        self.playlist = None
         self.stream = None
+        self.percent_downloaded = 0
 
-    def download_video(self, url):
+    def download_video(self, url) -> Status:
         if url == "":
-            return
+            return Status.NO_URL
 
-        self.video = YouTube(url)
-        self.stream = self.video.streams.get_highest_resolution()
-        self.stream.download()
+        try:
+            self.video = YouTube(url)
+        except VideoUnavailable:
+            return Status.UNAVAILABLE
+        else:
+            self.stream = self.video.streams.get_highest_resolution()
+            self.stream.download()
 
-    def download_audio(self, url, ext):
+        return Status.SUCCESS
+
+    def download_audio(self, url, ext) -> Status:
         if url == "":
-            return
+            return Status.NO_URL
 
-        self.video = YouTube(url)
-        self.stream = self.video.streams.get_audio_only()
-        downloaded_file = self.stream.download()
-        downloaded_path = os.path.splitext(downloaded_file)
-        audio_file = downloaded_path[0] + ext
-        os.rename(downloaded_file, audio_file)
+        try:
+            self.video = YouTube(url)
+        except VideoUnavailable:
+            return Status.UNAVAILABLE
+        else:
+            self.stream = self.video.streams.get_audio_only()
+            downloaded_file = self.stream.download()
+            # Once downloaded, we want to reformat the file to be a audio file. Below is the steps taken.
+            downloaded_path = os.path.splitext(downloaded_file)
+            audio_file = downloaded_path[0] + ext
+            os.rename(downloaded_file, audio_file)
+
+        return Status.SUCCESS
+
+    def download_playlist(self, url) -> Status:
+        if url == "":
+            return Status.NO_URL
+
+        download_status = Status.NONE
+        video_list = Playlist(url)
+        for link in video_list.video_urls:
+            download_status = self.download_video(link)
+
+        return download_status
